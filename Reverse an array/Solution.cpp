5d803d4cb3e163812dd3955873baa1ee50846896
Ankit Pathak <workwithankit2002@gmail.com>
2024-08-12 19:12:56 +0530

    Update Solution.cpp

--- Reverse an array/Solution.cpp
+++ Reverse an array/Solution.cpp
@@ -1,3 +1,5 @@
+// Solution 1
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -16,4 +18,39 @@ int main() {
     for(int i = 0; i < num; i++) {
         cout << array[i] << " ";
     }
+}
+
+
+// Solution 2
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void reverse(int arr[], int size) {
+    cin >> size;
+    for(int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+    int start = 0;
+    int end = size - 1;
+    while(start <= end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void print(int arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+int main() {
+    int arr[10000];
+    int size;
+    reverse(arr, size);
+    print(arr, size);
+    return 0;
 }
